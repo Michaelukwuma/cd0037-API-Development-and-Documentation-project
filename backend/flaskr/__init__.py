8a14f4dcc30e769e9d81f75dca7f270486c9962d
Michaelukwuma <ukwumamc@gmail.com>
2022-09-17 22:07:42 +0100

    third_update

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -155,39 +155,31 @@ def create_app(test_config=None):
             'total_questions': len(search_results),
             'current_category': None
         })
-
+        
         abort(404)
 
         #setting up a `POST` endpoint to get questions to play the quiz
 
         @app.route('/quizzes', methods=[POST])
-        def post_quizzes():
-        
-          try:
-            body = request.get_json()
-            
-            if not ('quiz_category'in body and 'previous_questions' in body):
-                abort(422)
-                
-            category = body.get('quiz_category')
-            previous_questions = body.get('previous_questions')
-            
-            if category['type'] == 'click':
-                current_questions = Question.query.filter(
-                    Question.id.notin_((previous_questions))).all()
-            else:
-                current_questions = Question.query.filter.by(
-                    category=category['id']).filter(Question.id.notin_((previous_questions))).all()
-                    
-                    
-            new_question = available_questions[random.randrange(
-                0, len(available_available))].format() if len(available_questions) > 0 else None
-            
+        def post_quizes():
+          body = request.get_json()
+          previous_question = body.get('question_text', None)
+          quiz_category = body.get('category', None)
+          
+          if new_question not in quiz_category:
+            abort(405)
+
+          if new_question == previous_question:
+            abort(405)
+          
+          else:
+            new_question = random.randint(question.query.filter_by(question.id).all())
+
             return jsonify({
-                'success': True,
-                'question': new_question
-            })
-          except:
-            abort(422)
+              'success': True,
+              'question': new_question
+              })
+
+          
 
         return app
